75b0a0ff9eb7b22b86ed6bb2bd094805bf89d336
mykoakbar <yeikoakbar20@gmail.com>
2026-04-03 20:39:50 +0700

    keri lurd

--- src/app/DataMaster/program-prioritas-pusat/Table.tsx
+++ src/app/DataMaster/program-prioritas-pusat/Table.tsx
@@ -37,6 +37,8 @@ const Table: React.FC<Table> = ({ tahun_akhir, tahun_awal }) => {
     const [ModalEdit, setModalEdit] = useState<boolean>(false);
     const [ModalBaru, setModalBaru] = useState<boolean>(false);
 
+    const [Dummy, setDummy] = useState<any[]>([]);
+    
     const handleModalEdit = (data: ProgramUnggulan | null) => {
         if (ModalEdit) {
             setModalEdit(false);
@@ -154,7 +156,7 @@ const Table: React.FC<Table> = ({ tahun_akhir, tahun_awal }) => {
                                 </td>
                             </tr>
                         ) : (
-                            Data.map((item: ProgramUnggulan, index: number) => (
+                            Dummy.map((item: ProgramUnggulan, index: number) => (
                                 <tr key={index}>
                                     <td className="border-x border-b border-green-500 py-4 px-3 text-center">{index + 1}</td>
                                     <td className="border-r border-b border-green-500 px-6 py-4 font-semibold">{item.nama_program_unggulan || "-"}</td>
