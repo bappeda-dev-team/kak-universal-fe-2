2b887577cecf5227b77cb7dcefb8df234f3df8c2
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2026-04-07 02:33:59 +0700

    show nama program prioritas

--- src/components/lib/Pohon/Opd/PohonOpd.tsx
+++ src/components/lib/Pohon/Opd/PohonOpd.tsx
@@ -846,7 +846,7 @@ export const TablePohon = (props: any) => {
                             <div className="flex flex-wrap items-center gap-2 mb-1">
                                 {tg?.keterangan_tagging_program?.map((tp: KeteranganTagging, tp_index: number) => (
                                     <h1 key={tp_index} className="py-1 px-3 text-start text-white bg-yellow-500 rounded-lg">
-                                        {tg.keterangan_tagging_program.length > 1 && `${tp_index + 1}.`} {tp.keterangan_tagging_program || ""}
+                                        {tg.keterangan_tagging_program.length > 1 && `${tp_index + 1}.`} {tp.nama_program_prioritas || ""}
                                     </h1>
                                 ))}
                             </div>
