af92b320b64b10a8df5458999b3c2eb48d582780
mykoakbar <yeikoakbar20@gmail.com>
2026-04-06 14:52:24 +0700

    add table tujuan sasaran rkpd dummy

--- src/app/RKPD/ranwal/sasaran-pemda/page.tsx
+++ src/app/RKPD/ranwal/sasaran-pemda/page.tsx
@@ -3,6 +3,7 @@
 import { FiHome } from "react-icons/fi";
 import Maintenance from "@/components/global/Maintenance";
 import { useBrandingContext } from "@/context/BrandingContext";
+import TableSasaran from "../../comp/TableSasaran";
 
 const RKPDRanwalSasaranPage = () => {
 
@@ -24,7 +25,11 @@ const RKPDRanwalSasaranPage = () => {
                         <h1 className="uppercase font-bold ml-1">{branding?.tahun?.label || ""}</h1>
                     </div>
                 </div>
-                <Maintenance />
+                {/* <Maintenance /> */}
+                <TableSasaran 
+                    tahun={branding?.tahun?.value || 0}
+                    menu="ranwal"
+                />
             </div>
         </>
     )
