b02b860f8ba758e9518cd41ecd2cc18afc99ea80
mykoakbar <yeikoakbar20@gmail.com>
2026-05-18 21:45:32 +0700

    add IKD

--- src/app/Renstra/ikd/comp/Table.tsx
+++ src/app/Renstra/ikd/comp/Table.tsx
@@ -5,9 +5,11 @@ import { LoadingClip } from "@/components/global/Loading";
 import { OpdNull, TahunNull } from "@/components/global/OpdTahunNull";
 import { getToken } from "@/components/lib/Cookie";
 import { useBrandingContext } from "@/context/BrandingContext";
-import { ButtonBlackBorder } from "@/components/global/Button";
-import { TbRefresh } from "react-icons/tb";
-import { IkdFindall, SasaranOPD, IndikatorSasaran, Pelaksana, ProgramOPD } from "../type";
+import { ButtonBlackBorder, ButtonSkyBorder } from "@/components/global/Button";
+import { TbRefresh, TbTrash } from "react-icons/tb";
+import { IkdFindall, SasaranOPD, Pelaksana, ProgramOPD } from "../type";
+import { ModalProgramIKD } from "./ModalProgramIKD";
+import { AlertQuestion, AlertNotification } from "@/components/global/Alert";
 
 interface Table {
     kode_opd: string;
@@ -96,7 +98,6 @@ export const Table: React.FC<Table> = ({ kode_opd, tahun }) => {
                 )
         }
     }
-
     const refresh = () => {
         setFetchTrigger((prev) => !prev);
     }
@@ -140,6 +141,7 @@ export const Table: React.FC<Table> = ({ kode_opd, tahun }) => {
                                     <th className="border-r border-b px-6 py-3 min-w-[250px]">Nama Pohon</th>
                                     <th className="border-r border-b px-6 py-3 min-w-[200px]">Pelaksana</th>
                                     <th className="border-l border-b px-6 py-3 min-w-[200px]">Sasaran OPD</th>
+                                    <th className="border-l border-b px-6 py-3 min-w-[150px]">Aksi</th>
                                     <th className="border-l border-b px-6 py-3 min-w-[200px]">Program OPD</th>
                                 </tr>
                                 <tr className="bg-emerald-700 text-white">
@@ -148,6 +150,7 @@ export const Table: React.FC<Table> = ({ kode_opd, tahun }) => {
                                     <th className="border-r border-b px-2 py-1 text-center">3</th>
                                     <th className="border-r border-b px-2 py-1 text-center">4</th>
                                     <th className="border-l border-b px-2 py-1 text-center">5</th>
+                                    <th className="border-l border-b px-2 py-1 text-center">6</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -161,14 +164,14 @@ export const Table: React.FC<Table> = ({ kode_opd, tahun }) => {
                                     Data.map((item: IkdFindall, index: number) => (
                                         <React.Fragment key={index}>
                                             <tr>
-                                                <td rowSpan={item.program_opd.length != 0 ? item.program_opd.length + 1 : 2} className="border border-emerald-500 px-4 py-4 text-center">{index + 1}</td>
-                                                <td rowSpan={item.program_opd.length != 0 ? item.program_opd.length + 1 : 2} className="border-x border-b border-emerald-500 px-6 py-4">
+                                                <td className="border border-emerald-500 px-4 py-4 text-center">{index + 1}</td>
+                                                <td className="border-x border-b border-emerald-500 px-6 py-4">
                                                     <div className="flex flex-col gap-1">
                                                         {item.nama_pohon || ""}
                                                         {JenisPohon(item.jenis_pohon || "")}
                                                     </div>
                                                 </td>
-                                                <td rowSpan={item.program_opd.length != 0 ? item.program_opd.length + 1 : 2} className="border-x border-b border-emerald-500 px-6 py-4">
+                                                <td className="border-x border-b border-emerald-500 px-6 py-4">
                                                     {item.pelaksana.length != 0 ?
                                                         <div className="flex flex-col items-center gap-3">
                                                             {item.pelaksana.map((p: Pelaksana, p_index) => (
@@ -179,32 +182,20 @@ export const Table: React.FC<Table> = ({ kode_opd, tahun }) => {
                                                             ))}
                                                         </div>
                                                         :
-                                                        <p className="text-red-500">Pelaksana Belum di pilih</p>
+                                                        <p className="text-red-300">Pelaksana Belum di pilih</p>
                                                     }
                                                 </td>
-                                                <td rowSpan={item.program_opd.length != 0 ? item.program_opd.length + 1 : 2} className="border-x border-b border-emerald-500 px-6 py-4">
+                                                <td className="border-x border-b border-emerald-500 px-6 py-4">
                                                     {item.sasaran_opd.length != 0 ?
                                                         item.sasaran_opd.map((s: SasaranOPD, s_index) => (
                                                             <p key={s_index}>{s.nama_sasaran_opd || ""}</p>
                                                         ))
                                                         :
-                                                        <p className="text-red-500">Sasaran OPD belum di buat</p>
+                                                        <p className="text-red-300">Sasaran OPD belum di buat</p>
                                                     }
                                                 </td>
+                                                <TrProgram Data={item} kode_opd={kode_opd} Program={item.program_opd_terpilih || []} />
                                             </tr>
-                                            {item.program_opd.length != 0 ?
-                                                item.program_opd.map((p: ProgramOPD, pr_index: number) => (
-                                                    <tr key={pr_index}>
-                                                        <td className="border-x border-b border-emerald-500 px-6 py-4">{p.nama_program || ""}</td>
-                                                    </tr>
-                                                ))
-                                                :
-                                                <tr>
-                                                    <td className="border-x border-b border-emerald-500 px-6 py-4">
-                                                        <p className="text-red-500">Tidak ada Program OPD</p>
-                                                    </td>
-                                                </tr>
-                                            }
                                         </React.Fragment>
                                     ))
                                 }
@@ -212,6 +203,120 @@ export const Table: React.FC<Table> = ({ kode_opd, tahun }) => {
                         </table>
                     </div>
                 </div>
+            </>
+        )
+    }
+}
+
+interface TrProgram {
+    Data: IkdFindall;
+    Program: ProgramOPD[];
+    kode_opd: string;
+}
+export const TrProgram: React.FC<TrProgram> = ({ Data, Program, kode_opd }) => {
+
+    const { branding } = useBrandingContext();
+    const token = getToken();
+    const [ProgramTerpilih, setProgramTerpilih] = useState<ProgramOPD[]>(Program || []);
+
+    const [DataIkd, setDataIkd] = useState<IkdFindall | null>(null);
+    const [ModalOpen, setModalOpen] = useState<boolean>(false);
+
+    const handleModal = (program: IkdFindall | null) => {
+        if (ModalOpen) {
+            setModalOpen(false);
+            setDataIkd(program);
+        } else {
+            setModalOpen(true);
+            setDataIkd(program);
+        }
+    }
+
+    const handleUpdate = (data: ProgramOPD) => {
+        setProgramTerpilih((prev) => [...prev, data]);
+    }
+
+    const hapusProgram = async (id: any) => {
+        try {
+            const response = await fetch(`${branding?.api_perencanaan}/ikd/select_program_opd/delete/${id}`, {
+                method: "DELETE",
+                headers: {
+                    Authorization: `${token}`,
+                    'Content-Type': 'application/json',
+                },
+            });
+            const data = await response.json();
+            if (data.code === 200) {
+                AlertNotification("Berhasil", "Program Di hapus", "success", 1000);
+                setProgramTerpilih(ProgramTerpilih.filter((data) => data.id !== id));
+            } else {
+                AlertNotification("Gagal", `${data.data}`, "error", 3000, true);
+            }
+        } catch (err) {
+            AlertNotification("Gagal", "cek koneksi internet atau database server", "error", 2000);
+            console.error(err);
+        }
+    };
+
+    if (ProgramTerpilih?.length != 0) {
+        return (
+            <>
+                <td className="border-x border-b border-emerald-500 px-6 py-4">
+                    <ButtonSkyBorder onClick={() => handleModal(Data)}>
+                        Tambah Program
+                    </ButtonSkyBorder>
+                </td>
+                <td className="border-x border-b border-emerald-500">
+                    {ProgramTerpilih.map((p: ProgramOPD, pr_index: number) => (
+                        <div className={`flex items-center gap-1 px-6 py-4 ${pr_index != ProgramTerpilih.length - 1 && "border-b border-emerald-500"}`} key={pr_index}>
+                            <p>{p.nama_program || ""}</p>
+                            <button
+                                type="button"
+                                className='flex items-center gap-1 rounded-full border border-red-500 p-1 text-red-500 hover:bg-red-300 hover:text-white'
+                                onClick={() => {
+                                    AlertQuestion("Hapus?", "Hapus Program?", "question", "Hapus", "Batal").then((result) => {
+                                        if (result.isConfirmed) {
+                                            hapusProgram(p.id);
+                                        }
+                                    });
+                                }}
+                            >
+                                <TbTrash />
+                            </button>
+                        </div>
+                    ))}
+                </td>
+                {ModalOpen &&
+                    <ModalProgramIKD
+                        isOpen={ModalOpen}
+                        onClose={() => handleModal(null)}
+                        Data={DataIkd}
+                        kode_opd={kode_opd}
+                        onSuccess={(data) => handleUpdate(data)}
+                    />
+                }
+            </>
+        )
+    } else {
+        return (
+            <>
+                <td className="border-x border-b border-emerald-500 px-6 py-4">
+                    <ButtonSkyBorder onClick={() => handleModal(Data)}>
+                        Tambah Program
+                    </ButtonSkyBorder>
+                </td>
+                <td className="border-x border-b border-emerald-500 px-6 py-4">
+                    <p className="text-red-300">Program OPD belum di pilih</p>
+                </td>
+                {ModalOpen &&
+                    <ModalProgramIKD
+                        isOpen={ModalOpen}
+                        onClose={() => handleModal(null)}
+                        Data={DataIkd}
+                        kode_opd={kode_opd}
+                        onSuccess={handleUpdate}
+                    />
+                }
             </>
         )
     }
