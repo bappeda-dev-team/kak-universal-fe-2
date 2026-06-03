69d4765dcc80eecdc22f961c990ff4de7a862a8c
mykoakbar <yeikoakbar20@gmail.com>
2026-06-04 00:39:31 +0700

    add cetak tematik

--- src/app/cetak/pokin-tematik/[id]/comp/PohonPemdaCetak.tsx
+++ src/app/cetak/pokin-tematik/[id]/comp/PohonPemdaCetak.tsx
@@ -1,65 +1,37 @@
 'use client'
 
-import '@/components/pages/Pohon/treeflex.css'
+// @ts-ignore: allow side-effect CSS import without type declarations
+import "../../../treeFlexCetak.css";
 import React, { useState } from 'react';
-import { TbCircleCheckFilled, TbCheck, TbCircleLetterXFilled, TbHourglass } from 'react-icons/tb';
 
 interface pohon {
     tema: any;
-}
-
-interface Tagging {
-    id: number;
-    id_pokin: number;
-    nama_tagging: string;
-    keterangan_tagging_program: KeteranganTagging[];
-}
-interface KeteranganTagging {
-    id: number;
-    id_tagging: number;
-    kode_program_unggulan: string;
-    keterangan_tagging_program: string;
-    tahun: string;
 }
 
 export const PohonPemdaCetak: React.FC<pohon> = ({ tema }) => {
 
     const [childPohons, setChildPohons] = useState(tema.childs || []);
 
-    console.log(tema);
-
     return (
         <li>
             <>
                 <div
-                    className={`tf-nc tf flex flex-col w-[600px] rounded-lg shadow-lg
-                                        ${tema.jenis_pohon === "Tematik" && 'shadow-slate-500'}
-                                        ${tema.jenis_pohon === "Sub Tematik" && 'shadow-slate-500'}
-                                        ${tema.jenis_pohon === "Sub Sub Tematik" && 'shadow-slate-500'}
-                                        ${tema.jenis_pohon === "Super Tematik" && 'shadow-slate-500'}
-                                        ${tema.jenis_pohon === "Strategic Pemda" && 'shadow-slate-500'}
-                                        ${tema.jenis_pohon === "Tactical Pemda" && 'shadow-slate-500'}
-                                        ${tema.jenis_pohon === "Operational Pemda" && 'shadow-slate-500'}
-                                        ${tema.jenis_pohon === "Strategic" && 'shadow-red-500 bg-red-700'}
-                                        ${tema.jenis_pohon === "Tactical" && 'shadow-blue-500 bg-blue-500'}
-                                        ${(tema.jenis_pohon === "Operational" || tema.jenis_pohon === "Operational N") && 'shadow-green-500 bg-green-500'}
-                                        ${tema.status === "ditolak" && 'shadow-black bg-gray-500'}
-                                    `}
+                    className={`tf-nc tf flex flex-col w-[600px] rounded-lg`}
                 >
                     {/* HEADER */}
                     <div
                         className={`flex pt-3 justify-center font-bold text-lg uppercase border my-3 py-3 rounded-lg bg-white
-                                            ${tema.jenis_pohon === "Tematik" && 'border-black'}
-                                            ${tema.jenis_pohon === "Sub Tematik" && 'border-black'}
-                                            ${tema.jenis_pohon === "Sub Sub Tematik" && 'border-black'}
-                                            ${tema.jenis_pohon === "Super Sub Tematik" && 'border-black'}
-                                            ${tema.jenis_pohon === "Strategic" && 'border-red-500 text-red-700'}
-                                            ${tema.jenis_pohon === "Tactical" && 'border-blue-500 text-blue-500'}
-                                            ${(tema.jenis_pohon === "Operational" || tema.jenis_pohon === "Operational N") && 'border-green-500 text-green-500'}
-                                            ${tema.jenis_pohon === "Strategic Pemda" && 'border-red-700 text-white bg-gradient-to-r from-[#CA3636] from-40% to-[#BD04A1]'}
-                                            ${tema.jenis_pohon === "Tactical Pemda" && 'border-blue-500 text-white bg-gradient-to-r from-[#3673CA] from-40% to-[#08D2FB]'}
-                                            ${tema.jenis_pohon === "Operational Pemda" && 'border-green-500 text-white bg-gradient-to-r from-[#139052] from-40% to-[#2DCB06]'}
-                                        `}
+                                                ${tema.jenis_pohon === "Tematik" && 'border-black'}
+                                                ${tema.jenis_pohon === "Sub Tematik" && 'border-black'}
+                                                ${tema.jenis_pohon === "Sub Sub Tematik" && 'border-black'}
+                                                ${tema.jenis_pohon === "Super Sub Tematik" && 'border-black'}
+                                                ${tema.jenis_pohon === "Strategic" && 'border-red-500 text-red-700'}
+                                                ${tema.jenis_pohon === "Tactical" && 'border-blue-500 text-blue-500'}
+                                                ${(tema.jenis_pohon === "Operational" || tema.jenis_pohon === "Operational N") && 'border-green-500 text-green-500'}
+                                                ${tema.jenis_pohon === "Strategic Pemda" && 'border-red-700 text-white bg-gradient-to-r from-[#CA3636] from-40% to-[#BD04A1]'}
+                                                ${tema.jenis_pohon === "Tactical Pemda" && 'border-blue-500 text-white bg-gradient-to-r from-[#3673CA] from-40% to-[#08D2FB]'}
+                                                ${tema.jenis_pohon === "Operational Pemda" && 'border-green-500 text-white bg-gradient-to-r from-[#139052] from-40% to-[#2DCB06]'}
+                                            `}
                     >
                         <div className="flex flex-wrap items-center justify-center gap-1">
                             <h1>{tema.jenis_pohon} {tema.id}</h1>
@@ -70,51 +42,32 @@ export const PohonPemdaCetak: React.FC<pohon> = ({ tema }) => {
                     </div>
                     {/* BODY */}
                     <div className="flex justify-center my-3">
-                            <TablePohon item={tema} />
+                        <TablePohon item={tema} />
                     </div>
                 </div>
             </>
             <ul>
                 {childPohons.map((dahan: any, index: any) => (
-                    <React.Fragment key={index}>
-                        <PohonPemdaCetak tema={dahan} />
-                    </React.Fragment>
+                    dahan.level_pohon > 3 ?
+                        <></>
+                        :
+                        <React.Fragment key={index}>
+                            <PohonPemdaCetak tema={dahan} />
+                        </React.Fragment>
                 ))}
             </ul>
         </li>
-        // <>cek</>
     )
 }
 
 export const TablePohon = (props: any) => {
     const tema = props.item.tema;
-    const tagging = props.item.tagging;
     const nama_pohon = props.item.nama_pohon;
     const keterangan = props.item.keterangan;
-    const opd = props.item.perangkat_daerah?.nama_opd;
     const jenis = props.item.jenis_pohon;
     const indikator = props.item.indikator;
-    const status = props.item.status;
     return (
         <div className="flex flex-col w-full">
-            {/* TAGGING */}
-            {tagging &&
-                tagging.map((tg: Tagging, tag_index: number) => (
-                    <div key={tag_index} className="flex flex-col gap-1 w-full px-3 py-1 border border-yellow-400 rounded-lg bg-white mb-2">
-                        <div className='flex items-center gap-1'>
-                            <h1 className='text-emerald-500'><TbCircleCheckFilled /></h1>
-                            <h1 className='font-semibold'>{tg.nama_tagging || "-"}</h1>
-                        </div>
-                        <div className="flex flex-wrap items-center gap-2 mb-1">
-                            {tg?.keterangan_tagging_program?.map((tp: KeteranganTagging, tp_index: number) => (
-                                <h1 key={tp_index} className="py-1 px-3 text-white text-start bg-yellow-500 rounded-lg">
-                                    {tg.keterangan_tagging_program.length > 1 && `${tp_index + 1}.`} {tp.keterangan_tagging_program || ""}
-                                </h1>
-                            ))}
-                        </div>
-                    </div>
-                ))
-            }
             <table className='w-full'>
                 <tbody>
                     <tr>
@@ -347,42 +300,6 @@ export const TablePohon = (props: any) => {
                             </tr>
                         </>
                     }
-                    {opd &&
-                        <tr>
-                            <td
-                                className={`min-w-[100px] border px-2 py-1 bg-white text-start
-                                    ${jenis === "Tematik" && "border-black"}
-                                    ${jenis === "Sub Tematik" && "border-black"}
-                                    ${jenis === "Sub Sub Tematik" && "border-black"}
-                                    ${jenis === "Super Sub Tematik" && "border-black"}
-                                    ${jenis === "Strategic Pemda" && "border-black"}
-                                    ${jenis === "Strategic" && "border-red-700"}
-                                    ${jenis === "Tactical Pemda" && "border-black"}
-                                    ${jenis === "Tactical" && "border-blue-500"}
-                                    ${jenis === "Operational Pemda" && "border-black"}
-                                    ${(jenis === "Operational" || jenis === "Operational N") && "border-green-500"}    
-                                `}
-                            >
-                                Perangkat Daerah
-                            </td>
-                            <td
-                                className={`min-w-[300px] border px-2 py-3 bg-white text-start
-                                    ${jenis === "Tematik" && "border-black"}
-                                    ${jenis === "Sub Tematik" && "border-black"}
-                                    ${jenis === "Sub Sub Tematik" && "border-black"}
-                                    ${jenis === "Super Sub Tematik" && "border-black"}
-                                    ${jenis === "Strategic Pemda" && "border-black"}
-                                    ${jenis === "Strategic" && "border-red-700"}
-                                    ${jenis === "Tactical Pemda" && "border-black"}
-                                    ${jenis === "Tactical" && "border-blue-500"}
-                                    ${jenis === "Operational Pemda" && "border-black"}
-                                    ${(jenis === "Operational" || jenis === "Operational N") && "border-green-500"}    
-                                `}
-                            >
-                                {opd ? opd : "-"}
-                            </td>
-                        </tr>
-                    }
                     <tr>
                         <td
                             className={`min-w-[100px] border px-2 py-1 bg-white text-start rounded-bl-lg
@@ -417,59 +334,6 @@ export const TablePohon = (props: any) => {
                             {keterangan ? keterangan : "-"}
                         </td>
                     </tr>
-                    {status &&
-                        <tr>
-                            <td
-                                className={`min-w-[100px] border px-2 py-1 bg-white text-start rounded-bl-lg
-                                    ${jenis === "Tematik" && "border-black"}
-                                    ${jenis === "Sub Tematik" && "border-black"}
-                                    ${jenis === "Sub Sub Tematik" && "border-black"}
-                                    ${jenis === "Super Sub Tematik" && "border-black"}
-                                    ${jenis === "Strategic Pemda" && "border-black"}
-                                    ${jenis === "Strategic" && "border-red-700"}
-                                    ${jenis === "Tactical Pemda" && "border-black"}
-                                    ${jenis === "Tactical" && "border-blue-500"}
-                                    ${jenis === "Operational Pemda" && "border-black"}
-                                    ${(jenis === "Operational" || jenis === "Operational N") && "border-green-500"}    
-                                `}
-                            >
-                                Status
-                            </td>
-                            <td
-                                className={`min-w-[300px] border px-2 py-3 bg-white text-start rounded-br-lg
-                                    ${jenis === "Tematik" && "border-black"}
-                                    ${jenis === "Sub Tematik" && "border-black"}
-                                    ${jenis === "Sub Sub Tematik" && "border-black"}
-                                    ${jenis === "Super Sub Tematik" && "border-black"}
-                                    ${jenis === "Strategic Pemda" && "border-black"}
-                                    ${jenis === "Strategic" && "border-red-700"}
-                                    ${jenis === "Tactical Pemda" && "border-black"}
-                                    ${jenis === "Tactical" && "border-blue-500"}
-                                    ${jenis === "Operational Pemda" && "border-black"}
-                                    ${(jenis === "Operational" || jenis === "Operational N") && "border-green-500"}    
-                                `}
-                            >
-                                {status === 'menunggu_disetujui' ? (
-                                    <div className="flex items-center">
-                                        {status || "-"}
-                                        <TbHourglass />
-                                    </div>
-                                ) : status === 'disetujui' ? (
-                                    <div className="flex items-center text-green-500">
-                                        {status || "-"}
-                                        <TbCheck />
-                                    </div>
-                                ) : status === 'ditolak' ? (
-                                    <div className="flex items-center text-red-500">
-                                        {status || "-"}
-                                        <TbCircleLetterXFilled />
-                                    </div>
-                                ) : (
-                                    <span>{status || "-"}</span>
-                                )}
-                            </td>
-                        </tr>
-                    }
                 </tbody>
             </table>
         </div>
--- src/app/cetak/pokin-tematik/[id]/page.tsx
+++ src/app/cetak/pokin-tematik/[id]/page.tsx
@@ -4,17 +4,72 @@ import { ButtonBlackBorder } from "@/components/global/Button";
 import { TbPrinter } from "react-icons/tb";
 import { useParams } from "next/navigation";
 import { PohonPemdaCetak } from "./comp/PohonPemdaCetak";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { getToken } from "@/components/lib/Cookie";
 import { LoadingClip } from "@/components/global/Loading";
+import html2canvas from "html2canvas";
 
 const CetakPokinTematik = () => {
 
     const { id } = useParams();
     const [Pohon, setPohon] = useState<any>(null);
     const [Loading, setLoading] = useState<boolean>(false);
+    const [LoadingCetak, setLoadingCetak] = useState<boolean>(false);
 
     const token = getToken();
+    const modalRef = useRef<HTMLDivElement | null>(null);
+
+    const handleDownloadPdf = async () => {
+        if (!modalRef.current) return;
+
+        const elementsToHide = document.querySelectorAll(".hide-on-capture") as NodeListOf<HTMLElement>;
+        elementsToHide.forEach((el) => (el.style.display = "none"));
+
+        try {
+            setLoadingCetak(true);
+            const element = modalRef.current;
+            const canvas = await html2canvas(element, {
+                scale: 1, // Higher scale for better quality
+                // width: element.scrollWidth + 50, // Use full scrollable width
+                // height: element.scrollHeight + 250, // Use full scrollable height
+                windowWidth: element.scrollWidth + 250, // Force full width rendering
+                windowHeight: element.scrollHeight + 250, // Force full height rendering
+                // useCORS: true, // For cross-origin images
+            });
+
+            // Create a new canvas with extra padding
+            const paddingTop = 50 // Extra padding for the top of the canvas
+            const newCanvas = document.createElement("canvas");
+            newCanvas.width = canvas.width;
+            newCanvas.height = canvas.height + paddingTop;
+
+            const ctx = newCanvas.getContext("2d");
+            if (ctx) {
+                ctx.fillStyle = "white"; // Optional: Background color
+                ctx.fillRect(0, 0, newCanvas.width, newCanvas.height);
+                ctx.drawImage(canvas, 0, paddingTop);
+
+                //hitung posisi horizontal untuk centering
+                const horizontalOffset = (newCanvas.width - canvas.width) / 2;
+
+                // Gambar canvas di tengah horizontal
+                ctx.drawImage(canvas, horizontalOffset, paddingTop);
+            }
+
+            const imgData = newCanvas.toDataURL("image/png");
+            const link = document.createElement("a");
+            link.href = imgData;
+            link.download = `cek_tematik.jpg`;
+            link.click();
+        } catch (error) {
+            alert("Error capturing the element");
+            console.error("Error capturing the element:", error);
+        } finally {
+            // Ensure elements are restored even if an error occurs
+            elementsToHide.forEach((el) => (el.style.display = ""));
+            setLoadingCetak(false);
+        }
+    };
 
     useEffect(() => {
         const fetchTematikKab = async () => {
@@ -53,16 +108,20 @@ const CetakPokinTematik = () => {
         return (
             <div className="flex flex-col p-1">
                 <div className="flex items-center gap-1">
-                    <ButtonBlackBorder className="flex items-center gap-1">
+                    <ButtonBlackBorder
+                        className="w-full flex items-center gap-1"
+                        onClick={handleDownloadPdf}
+                    >
                         <TbPrinter />
                         Download
                     </ButtonBlackBorder>
                 </div>
-                <h1>Halaman Cetak Pokin</h1>
-                {Pohon === null ? 
+                {Pohon === null ?
                     <h1>Pohon Tidak Tersedia</h1>
-                :
-                    <PohonPemdaCetak tema={Pohon} />
+                    :
+                    <div ref={modalRef} className="tf-tree text-center mt-3">
+                        <PohonPemdaCetak tema={Pohon} />
+                    </div>
                 }
             </div>
         )
