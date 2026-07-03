f522724ecbeb743b04a4479e05ecbdbcfd6ffbb0
zulfikar1609 <83656229+zulfikar1609@users.noreply.github.com>
2026-07-03 20:05:04 +0700

    menambahkan tombol dan fungsi untuk klik cetak excel

--- src/app/(main)/RPJMD/strategi-arah-kebijakan/comp/Table.tsx
+++ src/app/(main)/RPJMD/strategi-arah-kebijakan/comp/Table.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import React from "react";
-import { ArahKebijakan, SasaranPemda, ArahKebijakanPemda } from "../type";
+import {
+  ArahKebijakan,
+  SasaranPemda,
+  StrategiPemda,
+  ArahKebijakanPemda,
+} from "../type";
 import { ButtonRedBorder } from "@/components/global/Button";
 import { TbEyeClosed } from "react-icons/tb";
 
@@ -69,50 +74,82 @@ const Table: React.FC<Table> = ({ Data }) => {
                   {item.sasaran_pemdas ? (
                     item.sasaran_pemdas.map(
                       (s: SasaranPemda, s_index: number) => (
-                        <tr key={s_index}>
-                          <td className="border-r border-b border-emerald-500 px-6 py-4">
-                            {s.sasaran_pemda || ""}
-                          </td>
-                          <td className="border-r border-b border-emerald-500 px-6 py-4">
-                            {s.strategi_pemda || ""}
-                          </td>
-                          {s.arah_kebijakan_pemdas ? (
-                            <td className="border-r border-b p-2 border-emerald-500">
-                              <div className="flex flex-col items-center gap-2">
-                                {s.arah_kebijakan_pemdas.map(
-                                  (
-                                    ar: ArahKebijakanPemda,
-                                    ar_index: number,
-                                  ) => (
-                                    <p
-                                      key={ar_index}
-                                      className="flex flex-col gap-2 p-1 border border-emerald-500 rounded-lg w-full"
+                        <React.Fragment key={s_index}>
+                          {s.strategi_pemdas && s.strategi_pemdas.length > 0 ? (
+                            s.strategi_pemdas.map(
+                              (st: StrategiPemda, st_index: number) => (
+                                <tr key={`${s_index}-${st_index}`}>
+                                  {st_index === 0 && (
+                                    <td
+                                      rowSpan={s.strategi_pemdas.length}
+                                      className="border-r border-b border-emerald-500 px-6 py-4"
                                     >
-                                      {ar_index + 1}.{" "}
-                                      {ar.arah_kebijakan_pemda || "-"}
-                                      <ButtonRedBorder className="flex items-center gap-1 text-sm">
-                                        <TbEyeClosed /> Sembunyikan
-                                      </ButtonRedBorder>
-                                    </p>
-                                  ),
-                                )}
-                              </div>
-                            </td>
+                                      {s.sasaran_pemda || "-"}
+                                    </td>
+                                  )}
+
+                                  <td className="border-r border-b border-emerald-500 px-6 py-4">
+                                    {st.strategi_pemda || "-"}
+                                  </td>
+
+                                  {st.arah_kebijakan_pemdas &&
+                                  st.arah_kebijakan_pemdas.length > 0 ? (
+                                    <td className="border-r border-b p-2 border-emerald-500">
+                                      <div className="flex flex-col items-center gap-2">
+                                        {st.arah_kebijakan_pemdas.map(
+                                          (
+                                            ar: ArahKebijakanPemda,
+                                            ar_index: number,
+                                          ) => (
+                                            <p
+                                              key={ar_index}
+                                              className="flex flex-col gap-2 p-1 border border-emerald-500 rounded-lg w-full"
+                                            >
+                                              {ar_index + 1}.{" "}
+                                              {ar.arah_kebijakan_pemda}
+                                              <ButtonRedBorder className="flex items-center gap-1 text-sm">
+                                                <TbEyeClosed />
+                                                Sembunyikan
+                                              </ButtonRedBorder>
+                                            </p>
+                                          ),
+                                        )}
+                                      </div>
+                                    </td>
+                                  ) : (
+                                    <td className="border-r border-b text-white border-emerald-500 bg-red-500 px-6 py-4">
+                                      Strategic OPD belum dibuat
+                                    </td>
+                                  )}
+                                </tr>
+                              ),
+                            )
                           ) : (
-                            <td className="border-r border-b border-emerald-500 bg-red-400 px-6 py-4 font-semibold text-white">
-                              Stategic OPD belum di buat
-                            </td>
+                            <tr>
+                              <td className="border-r border-b border-emerald-500 px-6 py-4">
+                                {s.sasaran_pemda}
+                              </td>
+
+                              <td
+                                colSpan={2}
+                                className="border-r border-b text-white border-emerald-500 bg-red-500 px-6 py-4"
+                              >
+                                Sasaran Pemda belum dibuat
+                              </td>
+                            </tr>
                           )}
-                        </tr>
+                        </React.Fragment>
                       ),
                     )
                   ) : (
-                    <td
-                      colSpan={3}
-                      className="border-r border-b border-emerald-500 px-6 py-4 font-semibold bg-red-300 text-white"
-                    >
-                      sasaran pemda belum di buat
-                    </td>
+                    <tr>
+                      <td
+                        colSpan={3}
+                        className="border-r border-b border-emerald-500 bg-red-500 px-6 py-4"
+                      >
+                        Sasaran OPD belum dibuat
+                      </td>
+                    </tr>
                   )}
                 </React.Fragment>
               ))
--- src/app/(main)/Renstra/strategi-arah-kebijakan/type.ts
+++ src/app/(main)/Renstra/strategi-arah-kebijakan/type.ts
@@ -1,33 +1,36 @@
 export interface StrategicArahKebijakan {
-    kode_opd: string;
-    nama_opd: string;
-    tahun: string;
-    isu_strategis_opds: IsuStrategisOpd[];
-    tujuan_opd: TujuanOpd[];
-    strategi_arah_kebijakan_opds: ArahKebijakan[];
+  kode_opd: string;
+  nama_opd: string;
+  tahun: string;
+  isu_strategis_opds: IsuStrategisOpd[];
+  tujuan_opd: TujuanOpd[];
+  strategi_arah_kebijakan_opds: ArahKebijakan[];
 }
 
 export interface IsuStrategisOpd {
-    nama_isu_strategis: string;
+  nama_isu_strategis: string;
 }
 
 export interface TujuanOpd {
-    id: number;
-    kode_opd: string;
-    tujuan: string;
+  id: number;
+  kode_opd: string;
+  tujuan: string;
 }
 
 export interface ArahKebijakan {
-    tujuan_opd: string;
-    sasaran_opds: SasaranOpd[];
+  tujuan_opd: string;
+  sasaran_opds: SasaranOpd[];
 }
 
 export interface SasaranOpd {
-    sasaran_opd: string;
-    strategi_opd: string;
-    arah_kebijakan_opds: ArahKebijakanOpd[];
+  sasaran_opd: string;
+  strategi_opds: StrategiOpd[];
 }
 
+export interface StrategiOpd {
+  strategi_opd: string;
+  arah_kebijakan_opds: ArahKebijakanOpd[];
+}
 export interface ArahKebijakanOpd {
-    arah_kebijakan_opd: string;
+  arah_kebijakan_opd: string;
 }
