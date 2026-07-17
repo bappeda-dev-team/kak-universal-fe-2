5b5d9b5c905e7d6d359191d5a8a965834796e6c6
mykoakbar <yeikoakbar20@gmail.com>
2026-07-17 17:06:10 +0700

    clearing

--- src/components/global/Sidebar.tsx
+++ src/components/global/Sidebar.tsx
@@ -494,12 +494,6 @@ export const Sidebar = ({ isZoomed, isOpen, toggleSidebar }: SidebarProps) => {
                   <span className={`${!isOpen && 'hidden'} origin-left duration-200`}>User OPD</span>
                 </li>
               </Link>
-              <Link href="/bidang-urusan-opd">
-                <li className={`flex items-center gap-x-2 cursor-pointer p-2 rounded-xl ${url === "/bidang-urusan-opd" ? "bg-white text-gray-800" : "hover:bg-slate-500"}`}>
-                  <TbFileDelta className="text-xl" />
-                  <span className={`${!isOpen && 'hidden'} text-sm origin-left duration-200`}>Bidang Urusan OPD</span>
-                </li>
-              </Link>
               <Link href="/subkegiatanopd">
                 <li className={`flex items-center gap-x-2 cursor-pointer p-2 rounded-xl ${url === "/subkegiatanopd" ? "bg-white text-gray-800" : "hover:bg-slate-500"}`}>
                   <TbFileCode2 className="text-xl" />
