0e9f7c3f49bd0fd39f0b20b00e61f037108ae6d6
mykoakbar <yeikoakbar20@gmail.com>
2026-09-25 00:16:52 +0700

    change input target renja to number

--- src/app/(main)/Renja/comp/ModalIndikatorRenja.tsx
+++ src/app/(main)/Renja/comp/ModalIndikatorRenja.tsx
@@ -5,8 +5,8 @@ import { TbCirclePlus, TbTrash } from "react-icons/tb";
 import { Controller, SubmitHandler, useForm, useFieldArray } from "react-hook-form";
 import { ButtonSky, ButtonRed, ButtonSkyBorder } from '@/components/global/Button';
 import { getToken } from "@/components/lib/Cookie";
-import { LoadingButtonClip, LoadingClip } from "@/components/global/Loading";
-import { AlertNotification, AlertQuestion } from "@/components/global/Alert";
+import { LoadingButtonClip } from "@/components/global/Loading";
+import { AlertNotification } from "@/components/global/Alert";
 import { useBrandingContext } from "@/context/BrandingContext";
 
 interface FormValue {
@@ -30,7 +30,7 @@ interface Target {
     indikator_id?: string;
     satuan: string;
     tahun: string;
-    target: string;
+    target: number;
 }
 
 interface modal {
@@ -54,7 +54,7 @@ export const ModalIndikatorRenja: React.FC<modal> = ({ isOpen, onClose, tujuan_i
                 rumus_perhitungan: "",
                 sumber_data: "",
                 target: [{
-                    target: "",
+                    target: 0,
                     satuan: "",
                     tahun: tahun,
                 }]
@@ -78,7 +78,7 @@ export const ModalIndikatorRenja: React.FC<modal> = ({ isOpen, onClose, tujuan_i
             rumus_perhitungan: item.rumus_perhitungan,
             sumber_data: item.sumber_data,
             target: item.target.map((t: Target) => ({
-                target: t.target,
+                target: Number(t.target),
                 satuan: t.satuan,
                 tahun: t.tahun,
             }))
@@ -248,7 +248,7 @@ export const ModalIndikatorRenja: React.FC<modal> = ({ isOpen, onClose, tujuan_i
                                                     </label>
                                                     <input
                                                         {...field}
-                                                        type="text"
+                                                        type="number"
                                                         className="border px-4 py-2 rounded-lg"
                                                         placeholder="Masukkan target"
                                                     />
@@ -279,7 +279,7 @@ export const ModalIndikatorRenja: React.FC<modal> = ({ isOpen, onClose, tujuan_i
                         <ButtonSkyBorder
                             type="button"
                             className="flex items-center gap-1"
-                            onClick={() => append({ indikator: "", definisi_operasional: "", rumus_perhitungan: "", sumber_data: "", target: [{ target: "", satuan: "", tahun: tahun }] })}
+                            onClick={() => append({ indikator: "", definisi_operasional: "", rumus_perhitungan: "", sumber_data: "", target: [{ target: 0, satuan: "", tahun: tahun }] })}
                         >
                             <TbCirclePlus />
                             Tambah Indikator
